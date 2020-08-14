e84cf4df97a2dd212547514d189b27a86f582dbf
Dimitri Tishchenko <dtishchenko@change.org>
2020-08-14 14:32:40 -0700

    4 taboo and don't re-search taboo

--- morpion.jl
+++ morpion.jl
@@ -1273,13 +1273,10 @@ function run()
     max_score = pool_score
     max_moves = moves
 
-    taboo_score_multiplier = 1
+    taboo_score_multiplier = 4
     # dimitri
 
     while(true)
-
-        
-
         if length(pool_index) == 0
             pool_index = copy(dump)
             pool_score = maximum(map(function(value)
@@ -1409,21 +1406,10 @@ function run()
                     println(max_score)
                     println(max_moves)
 
-                    filter!(function(p)
-                        (key, (visits, dna, moves)) = p
-                        score = length(moves)
-                      
-                        pool_index[key] =  (0, dna, moves)
-                        println(" t + $score")
-
-                        false
-                    end, taboo)
-
-                    pool_score = maximum(map(function(value)
-                        (visits, dna, moves) = value
-                        length(moves)
-                    end, values(pool_index)))
+                    
                 end
+
+
 
                 if eval_score > max_score
                     println("$evaluation_count. **** $eval_score ****")
