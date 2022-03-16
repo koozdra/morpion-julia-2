4defcd55a6224f3a95074cd48c17aa0414ee38cb
Dimitri Tishchenko <dtishchenko@change.org>
2022-03-15 21:02:28 -0700

    tune

--- morpion.jl
+++ morpion.jl
@@ -1469,13 +1469,13 @@ function run()
     back_accept = 5
     back_accept_reset_visits = 5
     current_source_back_accept = 0
-    taboo_score_multiplier = 3 * 3
+    taboo_score_multiplier = 10 * 3
     end_search_interval = 500
     current_source_score = 100
     reset_interaval = 1000000
 
     focus_interval = 100000
-    back_focus_score_min = -2
+    back_focus_score_min = -3
     back_focus_score_max = 0
 
 
