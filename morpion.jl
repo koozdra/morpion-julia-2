60fa837546d0f07b1b1b0a14c74657e4ad935e89
Dimitri Tishchenko <dtishchenko@change.org>
2022-03-31 17:13:19 -0700

    tune

--- morpion.jl
+++ morpion.jl
@@ -1634,7 +1634,7 @@ function run()
         test_score = length(test_moves)
         test_age = iteration - test_iteration_born
 
-        if test_visits < (test_score * 2) && low_visit_counter < low_visit_timeout
+        if test_visits < test_score && low_visit_counter < low_visit_timeout
             low_visit_counter += 1
         elseif low_visit_counter >= low_visit_timeout
             current_set_index += 1
