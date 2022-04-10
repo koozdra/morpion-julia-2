145b4c711e0026d220b50a5561f899248ab23c95
Dimitri Tishchenko <dtishchenko@change.org>
2022-04-10 12:13:42 -0700

    tune

--- morpion.jl
+++ morpion.jl
@@ -1547,7 +1547,7 @@ function run()
     low_visit_counter = 0
 
     focus_interval = 100000
-    back_focus_score_min = -8
+    back_focus_score_min = -5
     back_focus_score_max = 0
 
     current_source_score = 100
@@ -1707,7 +1707,7 @@ function run()
         low_visit_timeout = 1
 
         if test_visits < (test_score * 2) || test_score == current_source_score
-            low_visit_timeout = 100
+            low_visit_timeout = 10
         end
 
         if low_visit_counter < low_visit_timeout
