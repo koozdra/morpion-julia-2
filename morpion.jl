d9167c3f84307a1a5c9a34226737e1c8106f1a30
Dimitri Tishchenko <dtishchenko@change.org>
2022-05-14 08:35:25 -0700

    tune

--- morpion.jl
+++ morpion.jl
@@ -1537,7 +1537,7 @@ function run()
     back_accept = 5
     back_accept_reset_visits = 5
     current_source_back_accept = 0
-    taboo_score_multiplier = 3
+    taboo_score_multiplier = 12
     # taboo_visits = 100
     end_search_interval = 0
     current_source_score = 100
